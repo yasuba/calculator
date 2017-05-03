ea3aa34cdba0a93771ff7a3efa5dcd2fd2c51d7e
yasuba <mayadrivermd@gmail.com>
2017-05-03 17:05:36 +0100

    writing computeSolutions for polynomial

--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -2,31 +2,23 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    Signal {
-      b.apply()*b.apply() - 4*a.apply()*c.apply()
-    }
-
-    // 5x² + 6x + 1 = 0
-    // a = 5, b = 6, c = 1
-    // 36 - 20
-    // 16
+      c: Signal[Double]): Signal[Double] = Signal {
+      b()*b() - 4*a()*c()
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    //    when b2 - 4ac is positive, we get two Real solutions
-    //    when it is zero we get just ONE real solution (both answers are the same)
-    //    when it is negative we get two Complex solutions
-
-//    read https://www.mathsisfun.com/algebra/quadratic-equation.html
-
-
-      // (-b ± √Δ) / 2a
-    if (delta.apply() % 2 == 0.0)
-      Signal {
-//        Set((-b.apply() + square root of delta ) / 2 * a.apply())
+    delta() match {
+      case d if d == 0.0 => Signal {
+          Set((-b() + math.sqrt(delta())) / (2 * a()))
+        }
+      case d if d > 0.0 => Signal {
+          Set((-b() + math.sqrt(delta())) / (2 * a()),
+            (-b() - math.sqrt(delta())) / (2 * a()))
+        }
+      case _ => Signal {
+        Set(0.0, 1.0) // why won't this work?
       }
-    ???
+    }
   }
 }
